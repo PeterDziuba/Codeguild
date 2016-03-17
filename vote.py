59018abc394ce66d5f34ca8d86fe2a613b5ff182
Peter Dziuba <PeterDziuba@gmail.com>
2016-03-17 14:50:39 -0700

    I made the voting program way cooler! Now it tells you who wins.

--- vote.py
+++ vote.py
@@ -7,7 +7,7 @@ while vote:
 	#We ask the user if they want to vote.
 	print("Would you like to vote?")
 	choice = input()
-	if (choice != 'n'):
+	if (choice == 'yes') | (choice == "Yes") | (choice == "y"):
 		#This adds votes to the total count and/or 
 		#adds candidates to the election dictionary.
 		print("Who would you like to vote for?")
@@ -19,4 +19,9 @@ while vote:
 	else:#This allows the user to stop voting when they are done.
 		vote = False
 
+
 print(election) #This prints out all the votes at the end!
+winner = (max(election, key=election.get, default="No one"))
+sorter = (sorted(election, key=election.get, reverse=True))
+print(winner, "wins!")
+print(sorter)
