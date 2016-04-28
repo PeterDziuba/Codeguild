57ea4671a0fe0c88dbe696babca71a54c32673cf
Peter Dziuba <PeterDziuba@gmail.com>
2016-04-28 15:01:52 -0700

    added sumpairs and changed some website stuff.

---
+++ sum.js
@@ -0,0 +1,36 @@
+"use strict";
+
+var pair_list = [5, 7, 1, -1, 12, 3, 4, 6, 22, 11, 2, 0];
+
+var notContains =  function(a, obj) {
+    for (var i = 0; i < a.length; i++) {
+        if (a[i] === obj) {
+            return false;
+        }
+    }
+    return true;
+}
+
+var findSumPairs = function(list, list_length, sum) {
+    var func_pair_list = [];
+    var my_item_list = [];
+    for (var i=0; i<list_length; i++) {
+        for (var j=0; j<list_length; j++) {
+            if (sum - list[i] === list[j]) {
+                var my_list = [list[i], list[j]];
+                if((notContains(my_item_list, list[i])) && (notContains(my_item_list, list[j]))) {
+                    func_pair_list.push(my_list);
+                    my_item_list.push(list[i]);
+                    my_item_list.push(list[j]);
+                };
+            };
+        }
+    }
+    return func_pair_list
+};
+
+var my_list_length = pair_list.length;
+var my_sum = findSumPairs(pair_list, my_list_length, 10);
+for(var i = 0; i < my_sum.length; i++){
+    console.log(my_sum[i]);
+}
