5bcc682f6c14a9fe07fe4bf728e4219fb249da56
Peter Dziuba <PeterDziuba@gmail.com>
2016-05-05 10:28:39 -0700

    added working whack a mole game

--- mole.js
+++ mole.js
@@ -1,1 +1,39 @@
 "use strict";
+
+function makeAMole() {
+    var newMole = $("<img src='mole.jpg' alt='mole'>");
+    newMole.on("click", function (event) {
+        event.preventDefault();
+        var newMoleHole = $("<img src='hole.jpg' alt='mole hole'>");
+        newMole.replaceWith(newMoleHole);
+    })
+    return newMole;
+}
+
+var moleSpots = ["#row1column1 img", "#row2column1 img", "#row3column1 img",
+                 "#row4column1 img", "#row1column2 img", "#row2column2 img",
+                 "#row3column2 img", "#row4column2 img", "#row1column3 img",
+                 "#row2column3 img", "#row3column3 img", "#row4column3 img",
+                 "#row1column4 img", "#row2column4 img", "#row3column4 img",
+                 "#row4column4 img", "#row1column5 img", "#row2column5 img",
+                 "#row3column5 img", "#row4column5 img"]
+
+function dropAMole(location) { //Use this to put the mole on our grid/board
+    var newMole = makeAMole();
+    location.replaceWith(newMole);
+}
+function whereToDrop () {
+   var myIndex = Math.floor((Math.random() * 19) + 0);
+   var myLocation = moleSpots[myIndex];
+   console.log(myLocation);
+   myLocation = $(myLocation);
+   dropAMole(myLocation);
+}
+
+function intervalDrop () {
+    setInterval(whereToDrop, 3000);
+}
+
+$(document).ready( function() {
+    intervalDrop();
+});
